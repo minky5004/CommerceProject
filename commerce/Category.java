63e99ad54a7cd5c6e0229fba7b0bdd10f6670ad9
minky5004 <minky5004@gmail.com>
2025-12-19 19:58:36 +0900

    Category 클래스 Customer 클래스 Done Todo CommerceSystem 보완

--- commerce/Category.java
+++ commerce/Category.java
@@ -1,4 +1,28 @@
 package commerce;
 
+import java.util.List;
+
 public class Category {
+
+    // 카테고리의 종류
+    private String category;
+
+    // CommerceSystem이 아닌 Category 메소드에서 List<Product>를 관리하기 위해서 새로 선언
+    private List<Product> products;
+
+    // 카테고리 객체 생성자
+    public Category(String category, List<Product> products) {
+        this.category = category;
+        this.products = products;
+    }
+
+    // 카테고리 이름을 반환하는 메소드
+    public String getCategoryName() {
+        return category;
+    }
+
+    // 프로덕트 반환
+    public List<Product> getProducts() {
+        return products;
+    }
 }
---
+++ commerce/Customer.java
@@ -0,0 +1,16 @@
+package commerce;
+
+public class Customer {
+
+    private String customerName;
+    private String customerEmail;
+    private String customerPrice;
+
+    public Customer(String customerName, String customerEmail, String customerPrice) {
+        this.customerName = customerName;
+        this.customerEmail = customerEmail;
+        this.customerPrice = customerPrice;
+    }
+
+
+}
