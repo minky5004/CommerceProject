6acca75a05acc3a932e2d7e16ba565e215d246e7
minky5004 <minky5004@gmail.com>
2025-12-22 19:43:38 +0900

    Step 3 CommerceSystem 카테고리 선택 기능 구현

--- commerce/CommerceSystem.java
+++ commerce/CommerceSystem.java
@@ -21,14 +21,41 @@ public class CommerceSystem {
             for(int i = 0; i < categories.size(); i++) {
                 System.out.println((i + 1) + ". " + categories.get(i).getCategoryName());
             }
-            System.out.println("0. 종료           | 프로그램 종료");
+            System.out.println("0. 종료           | 프로그램 종료\n");
 
             int mainSelect = stdin.nextInt();
 
             if (mainSelect == 0) {
                 System.out.println("프로그램 종료");
                 break;
+            }
+            else if (mainSelect > 0 && mainSelect <= categories.size()) {
+                Category category = categories.get(mainSelect - 1);
 
+                while (true) {
+                    System.out.println("\n[ " + category.getCategoryName() + " 카테고리 ]");
+                    List<Product> products = category.getProducts();
+
+                    for (int i = 0; i < products.size(); i++) {
+                        Product p = products.get(i);
+                        System.out.println((i + 1) + ". " + p.name + " | " + p.price + "원 | " + p.explanation);
+                    }
+                    System.out.println("0. 뒤로가기");
+
+                    int productSelect = stdin.nextInt();
+
+                    if (productSelect == 0) break; // 뒤로가기 (메인으로)
+
+                    if (productSelect > 0 && productSelect <= products.size()) {
+                        Product p = products.get(productSelect - 1);
+                        System.out.println("\n선택한 상품: " + p.name + " | " + p.price + "원 | " + p.explanation + " | 재고: " + p.stock + "개");
+                        break;
+                    } else {
+                        System.out.println("잘못된 상품 번호입니다.");
+                    }
+                }
+            } else {
+                System.out.println("잘못된 카테고리 번호입니다.");
             }
         }
     }
